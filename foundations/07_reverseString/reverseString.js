7b66c2e79a087864db3967c7d9d5ffa22cdd67df
abubeker ahmed <abubekra04@gmail.com>
2026-03-07 07:11:37 +0300

    task completed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let revesed="";
+    for (let index = word.length-1; index>=0; index--) {
+        revesed+= word[index];
+        
+    }
+    return revesed;
 };
 
 // Do not edit below this line
