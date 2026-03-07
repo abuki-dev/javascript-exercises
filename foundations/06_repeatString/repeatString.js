2a7ccf003ff70b1fe96cf943791b0eb2128ebdab
abubeker ahmed <abubekra04@gmail.com>
2026-03-07 06:45:55 +0300

    task completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string,num) {
+    if(num<0){
+        return "ERROR";
+    }
+    let word='';
+    let i=0;
+    while(i<num){
+        word+=string;
+        i++;
+    }
+    return word;
+}
 // Do not edit below this line
 module.exports = repeatString;
