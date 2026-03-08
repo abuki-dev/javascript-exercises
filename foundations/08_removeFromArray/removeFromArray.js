01b08696cae754827ebd73eca986e0233a5f8d28
abubeker ahmed <abubekra04@gmail.com>
2026-03-08 06:30:49 +0300

    task completed

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    const newarray=[];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if(!args.includes(element)){
+            newarray.push(element); 
+            
+        }
+    }
+    return newarray;
+
 };
 
 // Do not edit below this line
